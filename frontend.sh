30e32f3c2152791da92d8b5127c8f1bd103398bb
IT1089778 <nvrnagella@gmail.com>
2023-04-10 16:24:57 +0530

    installed nignx and start& enable nginx, download nginx add content, remove old nginx content then extract.

--- frontend.sh
+++ frontend.sh
@@ -4,3 +4,22 @@ print_head "installing nginx"
 yum install nginx -y &>> ${LOG}
 status_check
 
+print_head "start the nginx"
+systemctl start nginx &>> ${LOG}
+status_check
+
+print_head "enable nginx"
+systemctl enable nginx &>> ${LOG}
+status_check
+
+print_head "download the app content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> ${LOG}
+
+print_head "remove old app content"
+rm -rf /usr/share/nginx/html/* &>> ${LOG}
+status_check
+
+print_head "extracting nginx content"
+unzip /tmp/frontend.zip
+status_check
+
