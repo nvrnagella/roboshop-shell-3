962108c170d25d8223f869921b9ad97e4e168ebe
IT1089778 <nvrnagella@gmail.com>
2023-04-10 16:50:17 +0530

    mongodb script file ready and added mongodb repo file also

---
+++ mongodb.sh
@@ -0,0 +1,25 @@
+source common.sh
+
+print_head "setup repo for mongodb"
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+status_check
+
+print_head "install mongodb"
+yum install mongodb-org &>> ${LOG}
+status_check
+
+print_head "enable mongodb"
+systemctl enable mongod &>> ${LOG}
+status_check
+
+print_head "start mongodb"
+systemctl start mongod
+status_check
+
+print_head "update listed address"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+status_check
+
+print_head "restart mongodb"
+systemctl restart mongod
+status_check
