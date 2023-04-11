5b4b99bb02107c6a2909b8b8e242ca2310399951
IT1089778 <nvrnagella@gmail.com>
2023-04-11 07:15:39 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -9,7 +9,7 @@ dnf module enable redis:remi-6.2 -y &>> ${LOG}
 status_check
 
 print_head "install redis"
-yum intall redis -y &>> ${LOG}
+yum install redis -y &>> ${LOG}
 status_check
 
 print_head "enable redis"
